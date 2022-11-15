65274f58be4d79e8ea5fd4a783a2e873d69a2717
Martín García Ramos <mgarciara@fpcoruna.afundacion.org>
2022-11-15 01:51:54 +0100

    Ejercicio 5: Completado

--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/AboutActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/AboutActivity.java
@@ -3,23 +3,23 @@ package com.example.mycatalog;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class CatalogActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class AboutActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener  {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catalog);
+        setContentView(R.layout.activity_about);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
 
@@ -29,23 +29,16 @@ public class CatalogActivity extends AppCompatActivity implements NavigationView
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    public void navegarDetalle(View v) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        startActivity(intent);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.about_item) {
-            Intent intent = new Intent(this, AboutActivity.class);
-            startActivity(intent);
+        if (id == R.id.catalog_item) {
+            finish();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
